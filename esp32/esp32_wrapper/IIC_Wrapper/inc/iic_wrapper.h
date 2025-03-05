71e701cd7779bb6d47194972ee30613a2643e156
zht2649825643 <2649825643@qq.com>
2025-03-05 22:21:07 +0800

    [haitao] Add IIC Drv Code.

---
+++ esp32/esp32_wrapper/IIC_Wrapper/inc/iic_wrapper.h
@@ -0,0 +1,24 @@
+#ifndef _IIC_WRAPPER_H_
+#define _IIC_WRAPPER_H_
+
+typedef struct sf_i2c_bus_config{
+    unsigned int     sda_io_num;
+    unsigned int     scl_io_num;
+    unsigned int     clk_source;
+    unsigned int     i2c_port;
+    unsigned int     glitch_ignore_cnt;
+} sf_i2c_bus_config_t;
+
+typedef struct sf_i2c_device_config{
+    unsigned int     dev_addr_length;
+    unsigned int     device_address;
+    unsigned int     scl_speed_hz;
+} sf_i2c_device_config;
+
+typedef struct sf_i2c_config{
+    sf_i2c_bus_config_t     bus_config;
+    sf_i2c_device_config    device_config;
+} sf_i2c_config_t;
+
+
+#endif
